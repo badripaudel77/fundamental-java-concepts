3226f5e8aa6db63c00287ff21dbe23d8d7f2f1e2
Badri Paudel <40663560+badrivai@users.noreply.github.com>
2019-06-11 18:54:49 +0545

    Add files via upload
    Constructors

--- ThisAndChainConstructor.java
+++ ThisAndChainConstructor.java
@@ -1,13 +1,17 @@
 
 
 package corejava.OOPs.constructor;
+
 public class ThisAndChainConstructor {
+
     String name,course;
     int id;
     float fee;
+   
     public ThisAndChainConstructor(String name,String course,int id,float fee) {
-    this("Badri Paudel","Java",45);
-    this.fee=fee;
+  
+        this("Badri Paudel","Java",45);
+        this.fee=fee;
         System.out.println(name+" "+course+" "+id+" "+fee);
        
     
